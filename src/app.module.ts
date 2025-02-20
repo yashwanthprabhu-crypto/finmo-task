a490dabbf241001a26c5c3bf7af68ce437392f48
apnacollege <yashwanthprabhu2021@gmail.com>
2025-02-20 23:19:45 +0530

    modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ParkingModule } from './parking/parking.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 @Module({
   imports: [ParkingModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
